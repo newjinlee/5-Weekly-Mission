bf095559b2cb3adb6f976a6b02ad292e0f58a2e7
NEWJIN <winniepooh00@naver.com>
2024-03-27 17:41:06 +0900

    fix: signup.html 비밀번호 확인
    ...

--- Weekly-Mission/scripts/utils.js
+++ Weekly-Mission/scripts/utils.js
@@ -7,28 +7,34 @@ export const TEST_USER = {
   password: "codeit101",
 };
 
-export function setInputError(elements, message) {
-  elements.input.className += ` ${SIGN_INPUT_ERROR_CLASSNAME}`;
-  elements.errorMessage.className += ` ${ERROR_MESSAGE_CLASSNAME}`;
-  elements.errorMessage.textContent = message;
+
+/* 요소에 입력 오류 설정, 오류 메시지 표시 */
+export function setInputError(elements, errorMessage) {
+  elements.input.classList.add(SIGN_INPUT_ERROR_CLASSNAME);
+  elements.errorMessage.classList.add(ERROR_MESSAGE_CLASSNAME);
+  elements.errorMessage.textContent = errorMessage;
 }
 
+/* 요소에서 입력 오류 제거, 오류 메시지 숨기기 */
 export function removeInputError(elements) {
   elements.input.classList.remove(SIGN_INPUT_ERROR_CLASSNAME);
   elements.errorMessage.classList.remove(ERROR_MESSAGE_CLASSNAME);
   elements.errorMessage.textContent = "";
 }
 
+/* 이메일 유효성 확인 */
 export function isEmailValid(email) {
   return new RegExp(EMAIL_REGEX).test(email);
 }
 
+/* 비밀번호 유효성 확인 */
 export function isPasswordValid(password) {
   const isEightLettersOrMore = password.length >= 8;
   const hasNumberAndCharacter = password.match(/[0-9]/g) && password.match(/[a-zA-Z]/gi);
   return isEightLettersOrMore && hasNumberAndCharacter;
 }
 
+/* 엑세스 토큰이 로컬 스토리지에 저장되어 있는 지 확인 */
 export function redirectToIfAccessTokenExists(destination) {
   const accessToken = localStorage.getItem("accessToken");
   if (accessToken) {
